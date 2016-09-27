121341463b7e2b581acdf8c9181106a8385868e2
John Grubb <grubb@platform.sh>
2016-09-27 15:59:53 -0400

    formatting - indentation

--- lib/platform_sh.rb
+++ lib/platform_sh.rb
@@ -3,28 +3,28 @@ require "base64"
 require 'json'
 
 class PlatformSH
-  'use strict';
-   # Reads Platform.sh configuration from environment and returns a single object
-   def self.config
-      if ENV.has_key? "PLATFORM_PROJECT" then
-        conf = {}
-        conf["application"] = read_base64_json('PLATFORM_APPLICATION')
-        conf["relationships"] = read_base64_json('PLATFORM_RELATIONSHIPS')
-        conf["variables"] = read_base64_json('PLATFORM_VARIABLES')
-        conf["application_name"] =ENV["PLATFORM_APPLICATION_NAME"] || nil
-        conf["app_dir"] =ENV["PLATFORM_APP_DIR"] || nil
-        conf["document_root"] =ENV["PLATFORM_DOCUMENT_ROOT"] || nil
-        conf["environment"] =ENV["PLATFORM_ENVIRONMENT"] || nil
-        conf["project_entropy"] =ENV["PLATFORM_PROJECT_ENTROPY"] || nil
-        conf["project"] =ENV["PLATFORM_PROJECT"] || nil
-        conf["port"] =ENV["PORT"] || nil
-        conf["socket"] =ENV["SOCKET"] || nil
-      else
-        $stderr.puts "This is not running on platform.sh"
-        return nil
-      end
-      return conf;
+  'use strict'
+  # Reads Platform.sh configuration from environment and returns a single object
+  def self.config
+    if ENV.has_key? 'PLATFORM_PROJECT'
+      conf = {}
+      conf["application"] = read_base64_json('PLATFORM_APPLICATION')
+      conf["relationships"] = read_base64_json('PLATFORM_RELATIONSHIPS')
+      conf["variables"] = read_base64_json('PLATFORM_VARIABLES')
+      conf["application_name"] =ENV["PLATFORM_APPLICATION_NAME"] || nil
+      conf["app_dir"] =ENV["PLATFORM_APP_DIR"] || nil
+      conf["document_root"] =ENV["PLATFORM_DOCUMENT_ROOT"] || nil
+      conf["environment"] =ENV["PLATFORM_ENVIRONMENT"] || nil
+      conf["project_entropy"] =ENV["PLATFORM_PROJECT_ENTROPY"] || nil
+      conf["project"] =ENV["PLATFORM_PROJECT"] || nil
+      conf["port"] =ENV["PORT"] || nil
+      conf["socket"] =ENV["SOCKET"] || nil
+    else
+      $stderr.puts "This is not running on platform.sh"
+      return nil
     end
+    conf
+  end
 
   private
   def self.read_base64_json(var_name)
